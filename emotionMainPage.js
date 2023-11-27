6360c665b51acb757205ee71a13b3ec823b1e548
thedarshanpgowda <CS115K41@ms.mcehassan.ac.in>
2023-11-27 09:17:12 +0530

    nmn

--- emotionMainPage.js
+++ emotionMainPage.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const img = [
-  "images/img1.jpg", "images/img2.jpg" , "images/img3.jpg" , "images/img4.jpg", "images/img5.jpg", "images/img6.jpg","images/img7.jpg","images/img8.jpg","images/img9.jpg","images/img10.jpg","images/img11.jpg"
+    "images/img1.jpg",
+    "images/img2.jpg",
+    "images/img3.jpg",
+    "images/img4.jpg",
+    "images/img5.jpg",
+    "images/img6.jpg",
+    "images/img7.jpg",
+    "images/img8.jpg",
+    "images/img9.jpg",
+    "images/img10.jpg",
+    "images/img11.jpg",
   ];
 
   const inputEmail = document.querySelector('input[type = "email"]');
@@ -26,8 +36,6 @@ document.addEventListener("DOMContentLoaded", () => {
     BlockforPassword.style.border = "2px black solid";
   });
 
-
-
   //images
   var a = [];
   function updateImages() {
@@ -38,12 +46,11 @@ document.addEventListener("DOMContentLoaded", () => {
       images[1].style.opacity = "1";
       var i = parseInt(Math.random() * img.length);
       if (i in a) {
-        if(a.length == img.length){
+        if (a.length == img.length) {
           a = [];
         }
         updateImages();
-      } 
-      else {
+      } else {
         a.push(i);
         images[0].innerHTML = `<img src="${img[i]}" alt="">`;
         images[1].innerHTML = `<img src="${img[i]}" alt="">`;
@@ -51,7 +58,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 200);
   }
   setInterval(updateImages, 5000);
-
 
   //change content based on hover
   li.forEach((element, i) => {
@@ -82,26 +88,33 @@ document.addEventListener("DOMContentLoaded", () => {
     SignupPage.style.display = "flex";
   });
 
-
   const myPassword = document.querySelector('input[type="password"]');
   const myEmail = document.querySelector('input[type="email"]');
-  const submit = document.getElementById('submit');
-  submit.addEventListener('click',(event)=>{
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (event) => {
     event.preventDefault();
-    if(myEmail.value == 'sdarshanpgowda2003@gmail.com' && myPassword.value == '8431676017')
-    {
-      window.location = 'https://thedarshanpgowda.github.io/complain/emotionalTracker.html'
+    if (
+      myEmail.value == "sdarshanpgowda2003@gmail.com" &&
+      myPassword.value == "8431676017"
+    ) {
+      window.location =
+        "https://thedarshanpgowda.github.io/complain/emotionalTracker.html";
+        return;
     }
-    if(myEmail.value == 'mithungowdahc12a@gmail.com' && myPassword.value == '8431676017')
-    {
-      window.location = 'https://thedarshanpgowda.github.io/complain/teachersDashBOard.html'
-    }
-    else if((myEmail.value == 'sdarshanpgowda2003@gmail.com' && myPassword.value == '8431676017') || (myEmail.value == 'mithungowdahc12a@gmail.com' && myPassword.value == '8431676017')){
-      SignupPage.style.display = 'flex'
-      Signupcontent = SignupPage.querySelector('.login');
-      Signupcontent.innerHTML = "You are not eligible to proceed further, if you're email is not registered, continue further by signing-up using your email-ID"
-    }
-    
-  })
+    if (
+      myEmail.value == "mithungowdahc12a@gmail.com" &&
+      myPassword.value == "8431676017"
+    ) {
+      window.location =
+        "https://thedarshanpgowda.github.io/complain/teachersDashBOard.html";
+        return;
 
+    }
+    else {
+      SignupPage.style.display = "flex";
+      Signupcontent = SignupPage.querySelector(".login");
+      Signupcontent.innerHTML =
+        "You are not eligible to proceed further, if you're email is not registered, continue further by signing-up using your email-ID";
+    }
+  });
 });
