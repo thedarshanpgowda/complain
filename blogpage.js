eac353d9a3ff8d4f35f867f2b613cc317e0190f1
thedarshanpgowda <CS115K41@ms.mcehassan.ac.in>
2023-11-28 21:24:24 +0530

    mnm

--- blogpage.js
+++ blogpage.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage != NULL) {
-    const k = localStorage.getItem("Mymessage");
+  var arr = [];
+      for(var i = 0; i< 100; i++){
+        arr.push(i);
+      }
+      
+  let k;
+  if (localStorage != null) {
+    k = localStorage.getItem('message')
   } else {
     k = ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
         delectus? A asperiores cum quam similique ipsam molestias
@@ -9,6 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
         ducimus! Voluptate totam laboriosam aliquam, in quam, minus,
         quibusdam commodi excepturi quaerat neque quod incidunt at
         aliquid?`;
+  }
+
+  function clearMessage(data) {
+    // console.log('hello')
+    data.value = "";
   }
 
   function changes() {
@@ -27,8 +38,9 @@ document.addEventListener("DOMContentLoaded", () => {
       if (e.target.classList.contains("SubmitMain")) {
         const formContainer = e.target
           .closest(".formContainer")
-          .querySelector('input[type="text"]').value;
-        appendMessage(e, formContainer);
+          .querySelector('input[type="text"]');
+        appendMessage(e, formContainer.value);
+        clearMessage(formContainer);
       }
       if (e.target.classList.contains("replyA")) {
         replyblock(e);
@@ -38,6 +50,9 @@ document.addEventListener("DOMContentLoaded", () => {
       }
       if (e.target.classList.contains("likeAfter")) {
         likeafterblock(e);
+      }
+      if (e.target.id === "Mycomment") {
+        extrafunction(e);
       }
     });
 
@@ -50,15 +65,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const secondaryComment = document.querySelectorAll(".secondaryComment");
     const person = document.querySelector(".person");
     const comment = document.querySelector(".comment");
-    comment.innerHTML = k;
+    const mainUserComment = document.querySelector(".mainUserComment");
 
-    function mainmessage(e) {
-      console.log(formContainer);
+    mainUserComment.addEventListener("keypress", (event) => {
+      if (event.key == "Enter") {
+        event.preventDefault();
+        var usercomment = mainUserComment.value;
+        console.log('pressed')
+        appendMessage(event, usercomment);
+        clearMessage(mainUserComment);
+      }
+    });
+
+    function extrafunction(e){
+      e.target.addEventListener('keypress',(s)=>{
+        console.log('within')
+        if(s.key == 'Enter'){
+          s.preventDefault();
+          let message = e.target.value;
+          appendMessage(e,message)
+          e.target.closest('.secondaryContentComment').style.display = 'none';
+        }
+      })
     }
+      
+
+    comment.innerHTML = k;
 
     function appendMessage(e, message) {
       if (e.target.classList.contains("Submit")) {
-        console.log("de;ter");
         const k = e.target.closest(".secondaryContentComment");
         k.style.display = "none";
       }
@@ -92,11 +127,15 @@ document.addEventListener("DOMContentLoaded", () => {
                         </div>
                 </div>
             </div>`;
-      //   const reply = e.target.closest(".secondaryContentComment");
-      //   reply.style.display = "none";
+
       const person = e.target.closest(".person");
       person.appendChild(newdiv);
-      localStorage.setItem("Mymessage", `${message}`);
+      
+      if (localStorage == null) {
+        localStorage.setItem(arr[0], `${message}`);
+      } else {
+        localStorage.setItem(arr[i++],message);
+      }
     }
 
     function replyblock(event) {
